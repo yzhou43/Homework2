4019b37ad25cd8f1467b2115ee4796ca68915c67
yzhou43 <77255875+yzhou43@users.noreply.github.com>
2021-07-29 16:17:07 +0800

    Add files via upload

---
+++ SyntaxAnalyzer.c
@@ -0,0 +1,351 @@
+/*This file is a lexical and syntax analyzer system for simple arithmetic expressions
+
+Addition				+
+Subtraction				-
+Multiplication			*
+Division				/
+Module					%
+Less than				<
+Greater than			>
+Equal To				=
+Not Equal To			!
+
+IDENT = [a-zA-Z]+[ a-zA-Z0-9]*
+INT_LIT = [0-9]+
+FLOAT_LIT = [0-9]+. [0-9]+
+
+<PROGRAM> ---> "{"  <STMT_LIST>   "}"
+<STMT_LIST> ---> < STMT >{ ; < STMT >}
+< STMT > --->  < EXPR > {(">"| "<" |"="|"!") < EXPR > }
+< EXPR > --->  < TERM >  {("+"|"-") < TERM >}
+< TERM > ---> < FACTOR > {("*"|"/"|"%") < FACTOR >}
+< FACTOR > --> IDENT | INT_LIT | FLOAT_LIT | "(" < STMT > ")"
+
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* Character classes */
+#define LETTER 1
+#define DIGIT 2
+#define DECI_POINT 3
+#define UNKNOWN 99
+
+/* Token codes */
+#define INT_LIT 10
+#define IDENT 11
+#define FLOAT_LIT 12
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define MOD_OP 25
+#define LEFT_PAREN 26
+#define RIGHT_PAREN 27
+#define EQU_OP 28
+#define UNE_OP 29
+#define LES_OP 30
+#define GRE_OP 31
+#define SEMI 32
+#define LEFT_BRAC 33
+#define RIGHT_BRAC 34
+
+/* Global declarations */
+/* Variables */
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+//Store the tokens for following syntax analyzer
+int tokens[100];
+int i = 0;
+//Check if there are invalid characters in the string
+int length;
+int validLength = 0;
+
+FILE *in_fp;
+
+/* Function declarations */
+int lookup(char ch);
+void addChar(void);
+void getChar(void);
+void getNonBlank(void);
+int lex(void);
+void error();
+void stmtlist();
+void stmt();
+void expr();
+void term();
+void factor();
+
+
+//<PROGRAM> ---> "{"  <STMT_LIST>   "}"
+void program(void) {
+	if (nextToken != LEFT_BRAC)
+		error();
+	else {
+		lex();
+		stmtlist();
+		if (nextToken != RIGHT_BRAC)
+			error();
+	}
+}
+
+//<STMT_LIST> ---> < STMT >{ ; < STMT >}
+void stmtlist(void) {
+	stmt();
+	while (nextToken == SEMI) {
+		lex();
+		stmt();
+	}
+}
+
+//< STMT > --->  < EXPR > {(">"| "<" |"="|"!") < EXPR > }
+void stmt(void) {
+	expr();
+	while (nextToken == LES_OP || nextToken == GRE_OP ||
+	        nextToken == EQU_OP || nextToken == UNE_OP) {
+		lex();
+		expr();
+	}
+}
+
+//< EXPR > --->  < TERM >  {("+"|"-") < TERM >}
+void expr() {
+	term();
+	while (nextToken == ADD_OP || nextToken == SUB_OP) {
+		lex();
+		term();
+	}
+}
+
+// < TERM > ---> < FACTOR > {("*"|"/"|"%") < FACTOR >}
+void term() {
+	factor();
+	while (nextToken == MULT_OP || nextToken == DIV_OP || nextToken == MOD_OP) {
+		lex();
+		factor();
+	}
+}
+
+//< FACTOR > --> IDENT | INT_LIT | FLOAT_LIT | "(" < STMT > ")"
+void factor() {
+	switch (nextToken) {
+		case IDENT:
+			lex();
+			break;
+		case INT_LIT:
+			lex();
+			break;
+		case FLOAT_LIT:
+			lex();
+			break;
+		case '(':
+			lex();
+			stmt();
+			if ( nextToken != ')')
+				error();
+			else
+				lex();
+			break;
+		default:
+			error();
+	}
+}
+
+void error() {
+	printf("ERROR!");
+	return;
+}
+
+/******************************************************/
+/* lookup - a function to look up operators and
+ parentheses and return the token */
+int lookup(char ch) {
+	switch (ch) {
+		case '(':
+			addChar();
+			nextToken = LEFT_PAREN;
+			break;
+		case ')':
+			addChar();
+			nextToken = RIGHT_PAREN;
+			break;
+		case '+':
+			addChar();
+			nextToken = ADD_OP;
+			break;
+		case '-':
+			addChar();
+			nextToken = SUB_OP;
+			break;
+		case '*':
+			addChar();
+			nextToken = MULT_OP;
+			break;
+		case '/':
+			addChar();
+			nextToken = DIV_OP;
+			break;
+		case '%':
+			addChar();
+			nextToken = MOD_OP;
+			break;
+		case '=':
+			addChar();
+			nextToken = EQU_OP;
+			break;
+		case '!':
+			addChar();
+			nextToken = UNE_OP;
+			break;
+		case '<':
+			addChar();
+			nextToken = LES_OP;
+			break;
+		case '>':
+			addChar();
+			nextToken = GRE_OP;
+			break;
+		case ';':
+			addChar();
+			nextToken = SEMI;
+			break;
+//		case '.':
+//			addChar();
+//			nextToken = DECI_POINT;
+//			break;
+		default:
+			addChar();
+			nextToken = EOF;
+			break;
+	}
+	return nextToken;
+}
+
+/******************************************************/
+/* addChar - a function to add nextChar to lexeme */
+void addChar(void) {
+	if (lexLen <= 98) {
+		lexeme[lexLen++] = nextChar;
+		lexeme[lexLen] = '\0';
+	} else
+		printf("Error - lexeme is too long \n");
+}
+
+
+/******************************************************/
+/* getChar - a function to get the next character of
+ input and determine its character class */
+void getChar(void) {
+	if ((nextChar = getc(in_fp)) != EOF) {
+		validLength++;
+		if (isalpha(nextChar))
+			charClass = LETTER;
+		else if (isdigit(nextChar))
+			charClass = DIGIT;
+		else if (nextChar == '.')
+			charClass = DECI_POINT;
+		else
+			charClass = UNKNOWN;
+	} else
+		charClass = EOF;
+}
+
+
+/******************************************************/
+/* getNonBlank - a function to call getChar until it
+ returns a non-whitespace character */
+void getNonBlank(void) {
+	while (isspace(nextChar))
+		getChar();
+}
+
+/******************************************************/
+/* lex - a simple lexical analyzer for arithmetic
+ expressions */
+int lex(void) {
+	lexLen = 0;
+	getNonBlank();
+	switch (charClass) {
+		/* Identifiers */
+		case LETTER:
+			addChar();
+			getChar();
+			while (charClass == LETTER || charClass == DIGIT) {
+				addChar();
+				getChar();
+			}
+			nextToken = IDENT;
+			break;
+		/* Integer literals */
+		case DIGIT:
+			addChar();
+			getChar();
+			while (charClass == DIGIT) {
+				addChar();
+				getChar();
+			}
+			nextToken = INT_LIT;
+			// Floating point literals
+			if (charClass == DECI_POINT) {
+				addChar();
+				getChar();
+				while (charClass == DIGIT) {
+					addChar();
+					getChar();
+				}
+				nextToken = FLOAT_LIT;
+			}
+			break;
+		/* Parentheses and operators */
+		case UNKNOWN:
+			lookup(nextChar);
+			getChar();
+			break;
+		/* EOF */
+		case EOF:
+			nextToken = EOF;
+			lexeme[0] = 'E';
+			lexeme[1] = 'O';
+			lexeme[2] = 'F';
+			lexeme[3] = '\0';
+			break;
+	} /* End of switch */
+	//printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
+	tokens[i] = nextToken;
+	i = i + 1;
+	return nextToken;
+} /* End of function lex */
+
+/******************************************************/
+/* main driver */
+int main(void) {
+	/* Open the input data file and process its contents */
+	if ((in_fp = fopen("input.txt", "r")) == NULL)
+		printf("ERROR - cannot open front.in \n");
+	else {
+		fseek(in_fp, 0, SEEK_END);
+		length  = ftell(in_fp);
+//		printf("%d",length);
+		in_fp = fopen("input.txt", "r");
+		printf("Lexical Analysis: ");
+		getChar();
+		do {
+			lex();
+		} while (nextToken != EOF);
+	}
+// Test if the input strings pass the lexical analysis
+// Conduct the syntax analysis only if the code is lexically correct
+	if (validLength == length - 1) {
+		printf("No lexical error\n\n");
+		printf("Syntax Analysis: ");
+		program();
+	} else
+		printf("ERROR!\n");
+	return 0;
+}
